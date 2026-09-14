a2edfa4b4e893cf5800a91d7a0bf7b5b8b9a5641
KyleBrooks <kyle.brooks1989@gmail.com>
2026-09-14 11:39:27 -0500

    add POST endpoint and setup a simulated DB locally

---
+++ db/db.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Could not connect to database")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+}
