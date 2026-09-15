4f0d289f8ec300bbe591e4d73fb55aa4e32fdefb
KyleBrooks <kyle.brooks1989@gmail.com>
2026-09-15 09:10:07 -0500

    add logic to delete a single event

--- routes/events.go
+++ routes/events.go
@@ -68,7 +68,7 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	_, err := models.GetEventById(eventId)
+	_, err = models.GetEventById(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
@@ -95,3 +95,29 @@ func updateEvent(context *gin.Context) {
 
 	context.JSON(http.StatusOK, gin.H{"message": "Event updated!", "event": updatedEvent})
 }
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID."})
+		return
+	}
+	
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully!"})
+}
+
