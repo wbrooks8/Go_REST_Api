867b0e44b561b7d1905f320846b331977f9c3f30
KyleBrooks <kyle.brooks1989@gmail.com>
2026-09-15 09:25:44 -0500

    add logic to create a user

---
+++ routes/users.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"net/http"
+
+	"example.com/REST-api/models"
+		"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	err = user.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create user."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created!", "user": user})
+
+
+}
