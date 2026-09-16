e0c299b907b10e570b5050d0c7bdec6e32c92a78
KyleBrooks <kyle.brooks1989@gmail.com>
2026-09-16 10:37:08 -0500

    added token checking logic

--- routes/events.go
+++ routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"example.com/REST-api/models"
+	"example.com/REST-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -39,8 +40,22 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
+	err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
@@ -103,7 +118,7 @@ func deleteEvent(context *gin.Context) {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID."})
 		return
 	}
-	
+
 	event, err := models.GetEventById(eventId)
 
 	if err != nil {
@@ -120,4 +135,3 @@ func deleteEvent(context *gin.Context) {
 
 	context.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully!"})
 }
-
--- utils/jwt.go
+++ utils/jwt.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"time"
+	"errors"
 
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -16,4 +17,39 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) error{
+	parsedToken, err :=jwt.Parse(token, func(token *jwt.Token) (any, error){
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Unexcpected signing method")
+		}
+
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return errors.New("Could not parse token")
+	}
+
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return errors.New("Token is invalid")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	// if !ok {
+	// 	return errors.New("Invalid token claims")
+	// }
+
+	// email :=claims["email"].(string)
+	// userId := claims["userId"].(int64)
+
+	return nil
+
+
 }
