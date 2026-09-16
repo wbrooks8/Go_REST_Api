c44df34e039c6153c0139db8f945f37847e56c24
KyleBrooks <kyle.brooks1989@gmail.com>
2026-09-16 11:11:12 -0500

    prevent users from updating events they don't own

--- routes/events.go
+++ routes/events.go
@@ -70,10 +70,16 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	_, err = models.GetEventById(eventId)
+	userId := context.GetInt64("userId")
+	event, err := models.GetEventById(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	if int64(event.UserID) != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to udpate event!"})
 		return
 	}
 
